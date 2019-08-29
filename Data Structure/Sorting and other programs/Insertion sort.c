c694658a0dce35c950549f7cc1bd920c1b608abd
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-08-29 23:04:25 +0530

    initial commit

---
+++ Data Structure/Sorting and other programs/Insertion sort.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int main()
+{
+    int arr[100],limit;
+    int i,j,temp;
+     
+    printf("Enter total number of elements: ");
+    scanf("%d",&limit);
+     
+    /*Read array*/
+    printf("Enter array elements: \n");
+    for(i=0; i<limit; i++)
+    {
+        printf("Enter element %3d: ",i+1);
+        scanf("%d",&arr[i]);
+    }
+     
+    /*sort elements in Ascending Order*/
+    for(i=1; i<(limit); i++)
+    {
+        j=i;
+        while(j>0 && arr[j]<arr[j-1])
+        {
+            temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+             
+            j--;
+        }
+    }
+ 
+    printf("Array elements in Ascending Order:\n");
+    for(i=0; i<limit; i++)
+        printf("%d ",arr[i]);
+     
+    printf("\n");
+         
+    for(i=1; i<(limit); i++)
+    {
+        j=i;
+        while(j>0 && arr[j]>arr[j-1])
+        {
+            temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+             
+            j--;
+        }
+    }
+ 
+    printf("Array elements in Descending Order:\n");
+    for(i=0; i<limit; i++)
+        printf("%d ",arr[i]);
+         
+    printf("\n");
+     
+    return 0;
+}
