27be68e3a16b54c0da1cc812442b229f6e8bcf1b
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-08-31 00:09:20 +0530

    initial commit

---
+++ Java/Class and object programs/Print message using class.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Show
+{
+    public void print()
+    {
+        System.out.println("Hello World."); 
+    }
+}
+ 
+public class main
+{
+    public static void main(String []s)
+    {
+        Show obj = new Show();
+        obj.print();
+    }
+}
