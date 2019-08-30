82b72572db9955e08d729df898b629ffe34479e7
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-08-30 23:56:38 +0530

    initial commit

---
+++ Java/Basic programs/Swap two number without using third variable.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class SwapTwoNumbers
+{
+	public static void main(String []s)
+	{
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+
+		System.out.print("Enter value of a: ");
+		a=sc.nextInt();
+		System.out.print("Enter value of a: ");
+		b=sc.nextInt();
+
+		System.out.println("Before swapping  a: "+ a +", b: " + b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("After swapping  a: "+ a +", b: " + b);
+	}
+}
