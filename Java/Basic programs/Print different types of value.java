b96a8b2646642c2a44f85cc81ccea744324bebf6
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-08-30 23:33:19 +0530

    initial commit

---
+++ Java/Basic programs/Print different types of value.java
@@ -0,0 +1,17 @@
+class main{
+    public static void main(String args[])
+    {
+        int num;
+        float b;
+        char c;
+        String s;
+        num=100;
+        b=1.234f;
+        c='A';
+        s="Hello world";
+        System.out.println("Value of num: "+num);
+        System.out.println("Value of b: "+b);
+        System.out.println("Value of c: "+c);
+        System.out.println("Value of s: "+s);
+    }
+}
---
+++ Java/Basic programs/Read and print an integer.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class main {
+    public static void main(String args[])
+    {
+        int a;
+        Scanner s=new Scanner(System.in);
+        a=s.nextInt();
+        System.out.println("Value of a : "+a);
+    }
+}
