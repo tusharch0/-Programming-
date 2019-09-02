ee5210f28335f165f5e003a94bd94c9e5dfb6391
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-09-02 19:59:53 +0530

    initial commit

--- Java/Basic programs/Find_square_cube_and_square_root_of_a_number.java
+++ Java/Basic programs/Find_square_cube_and_square_root_of_a_number.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class Find_square_cube_and_square_root_of_a_number
 {
 	public static void main(String args[]){
-		Scanner sc=new Scanner(System.in);
+		Scanner s=new Scanner(System.in);
 		int num;
 
 		System.out.print("Enter a number: ");
-		num=sc.nextInt();
+		num=s.nextInt();
 
 		System.out.println("Square of "+ num + " is: "+ Math.pow(num, 2));
 		System.out.println("Cube of "+ num + " is: "+ Math.pow(num, 3));
