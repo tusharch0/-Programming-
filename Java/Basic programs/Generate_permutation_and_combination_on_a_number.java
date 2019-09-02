c2df374849268bbc9dc53fbed7c40786c7c6d630
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-09-02 20:07:48 +0530

    initial commit

--- Java/Basic programs/Generate_permutation_and_combination_on_a_number.java
+++ Java/Basic programs/Generate_permutation_and_combination_on_a_number.java
@@ -1,5 +1,6 @@
 import java .util.*;
-public class main{
+public class Generate_permutation_and_combination_on_a_number
+{
     public static int fact(int num)
     {
         int fact =1,i;
@@ -14,9 +15,9 @@ public class main{
         int n,r;
         Scanner s=new Scanner (System.in);
         System.out.print ("Enter n: ");
-        n=s.nextInt ;
+        n=s.nextInt() ;
         System.out.print ("Enter r: ");
-        r=s.nextInt ;
+        r=s.nextInt() ;
         System.out.print("Combination is "+(fact (n)/(fact (r))));
         System.out.print("Permutation is "+(fact (n)/(fact (n-r))));
     }
