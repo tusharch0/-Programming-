406e883150961f4c69d74110c19ce597243cb346
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-09-03 23:26:13 +0530

    initial commit

---
+++ Java/Basic programs/Find_the_sum_of_all_digits.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Find_the_sum_of_all_digits
+{
+    private static Scanner s;
+
+    public static void main(String args[])
+    {
+    	// initializing and declaring the objects.
+        int num, rem=0, sum=0, temp;
+        s = new Scanner(System.in);
+		
+        // enter number here.
+        System.out.print("Enter the Number : ");
+        num = s.nextInt();
+		
+        // temp is to store number.
+        temp = num;
+		
+        while(num>0)
+        {
+            rem = num%10;
+            sum = sum+rem;
+            num = num/10;
+        }
+		
+        System.out.print("Sum of Digits of " +temp+ " is : " +sum);        
+    }
+}
