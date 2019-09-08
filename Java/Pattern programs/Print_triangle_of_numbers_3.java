92a20811a76cd8b2d1ea24261bd77c1347f48447
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-09-08 23:34:28 +0530

    initial commit

--- Java/Pattern programs/Print_triangle_of_numbers_3.java
+++ Java/Pattern programs/Print_triangle_of_numbers_3.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class Print_triangle_of_number_2 
+public class Print_trangle_of_numbers_3
 {
 	public static void main(String[] args) 
     {
@@ -9,15 +8,24 @@ public class Print_triangle_of_number_2
         System.out.print("Enter row for pattern : ");
          
         int rows = sc.nextInt();
-        
-        System.out.println("Here is your pattern....!!!");
          
-        for (int i = 1; i <= rows; i++) 
+        System.out.println("Here is your pattern....!!!");
+        
+        for (int i = rows; i >= 1; i--) 
         {
             for (int j = 1; j <= i; j++)
             {
                 System.out.print(j+" ");
-            }            
+            }      
+            System.out.println();
+        }
+        
+        for (int i = 2; i <= rows; i++) 
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(j+" ");
+            }             
             System.out.println();
         }
         sc.close();
