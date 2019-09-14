ea5c28469f7d46f62e3873b42a2b71b85c720b4f
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-09-14 22:59:27 +0530

    initial commit

---
+++ C/Goto programs/Print table of given no. using goto.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	int count,t,r;
+
+	printf("Enter number: ");
+	scanf("%d",&t);
+
+	count=1;
+
+	start:
+	if(count<=10)
+	{
+		r=t*count;
+		printf("%d*%d=%d\n",t,count,r);
+		count++;
+		goto start;
+	} 
+
+	return 0;
+}
