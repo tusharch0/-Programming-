d61028b1cc2b0758ff449d07ee8a2d4b9443fe2f
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-09-14 23:07:27 +0530

    initial commit

---
+++ C/Goto programs/Print square and cube of all number from 1 to n.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int count,n;
+
+	printf("Enter the value of N: ");
+	scanf("%d",&n);
+	
+	count=1;
+	
+	start:
+	printf("num: %4d, Square: %4d, Cube: %4d\n",count,(count*count),(count*count*count));
+	count++;
+
+	if(count<=n)
+		goto start;
+
+	return 0;
+}
