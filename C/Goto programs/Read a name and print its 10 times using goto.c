62e87ccf1125a234acf2234f80728987f62ed72b
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-09-14 23:02:30 +0530

    initial commit

---
+++ C/Goto programs/Read a name and print its 10 times using goto.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int count;
+	char name[50];
+
+	printf("Enter the name: ");
+	scanf("%s",name);
+
+	count=1;
+
+	start:
+	printf("%s, ",name);
+	count++;
+	if(count<=10)
+		goto start;
+
+	return 0;
+}
