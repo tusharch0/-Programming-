3fa9a8c1f09318bbcc56ad7cb7b191733b1a477e
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-09-18 22:28:02 +0530

    initial commit

---
+++ Java/Basic programs/Check_given_no_is_pronic_or_not.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Check_given_no_is_pronic_or_not 
+{
+	public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+         
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        
+        int flag = 0;
+    
+        for(int i=0; i<n; i++)
+        {
+            if(i*(i+1) == n)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        
+        if(flag == 1)
+            System.out.println(n+" is a Pronic Number.");
+        else
+            System.out.println(n+" is not a Pronic Number.");      
+    }
+}
