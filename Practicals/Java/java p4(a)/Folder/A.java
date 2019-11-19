6a610b0c524d710d6929745573e9c96cca632fab
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-11-19 23:27:14 +0530

    initial commit

---
+++ Practicals/Java/java p4(a)/Folder/A.java
@@ -0,0 +1,8 @@
+package Folder;
+public class A
+{
+public void display()
+  {
+    System.out.println("Method is public");
+  }
+}
---
+++ Practicals/Java/java p4(b)/Main.java
@@ -0,0 +1,13 @@
+// default
+import Folder.*;
+ 
+//This class is having default access modifier
+class Main
+{
+  public static void main(String args[])
+  {
+    //accessing class Geek from package p1
+    A obj = new A();
+    obj.display();
+  }
+}
