3e9395380d794e8415ed59b602f3da92f626382e
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-11-19 23:23:14 +0530

    initial commit

---
+++ Practicals/Java/java p3(a)/SingleInheretanceDemo.java
@@ -0,0 +1,19 @@
+// Single Inheretance 
+
+class Animal
+{  
+void eat(){System.out.println("eating...");}  
+}  
+class Dog extends Animal
+{  
+void bark(){System.out.println("barking...");}  
+}  
+class SingleInheretanceDemo
+{  
+public static void main(String args[])
+{  
+Dog d=new Dog();  
+d.bark();  
+d.eat();  
+}
+}  
