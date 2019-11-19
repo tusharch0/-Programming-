9417de1757eca3e257f9bf8c33f8ceda8868f274
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-11-19 23:29:13 +0530

    initial commit

---
+++ Practicals/Java/java p4(d)/Main.java
@@ -0,0 +1,12 @@
+// protected 
+import Folder.*; 
+ 
+//Class B is subclass of A
+class Main extends A
+{
+public static void main(String args[])
+{
+  Main obj = new Main();
+  obj.display();
+}
+}
