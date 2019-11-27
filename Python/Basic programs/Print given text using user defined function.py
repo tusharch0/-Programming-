a6673208b132fb813c2aa9d3fd849e1a6e150fe0
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-11-28 00:18:31 +0530

    initial commit

---
+++ Python/Basic programs/Print given text using user defined function.py
@@ -0,0 +1,8 @@
+def putMe(text):
+    print (text)
+
+if __name__ =='__main__':
+    putMe('Hello World ')
+    putMe('Welcome there')
+    putMe('this is tushar here')
+    putMe('End!')
---
+++ Python/Basic programs/Printing different values.py
@@ -0,0 +1,31 @@
+# printing Integer values 
+print(12)
+print(12+20)
+a=10
+print(a)
+print("Integer\t:",a)
+print("Integer\t:"+str(a))
+
+# printing Float values 
+print(12.89)
+print(12.89+34.98)
+b=76.97
+print(b)
+print("Float\t:",b)
+print("Float\t:"+str(b))
+
+# printing String values 
+print("Hello")
+print("Hello"+"World")
+c="hello"
+print(c)
+print("String\t:",c)
+print("String\t:"+str(c))
+
+# printing Boolean Values
+print(True)
+print(True+False)
+d=True
+print(d)
+print("Boolean\t:",d)
+print("Boolean\t:"+str(d))
