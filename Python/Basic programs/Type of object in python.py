c88d18febf5457fa830c0f7be6850b234c7f658f
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-02 00:02:51 +0530

    initial commit

---
+++ Python/Basic programs/Type of object in python.py
@@ -0,0 +1,24 @@
+# Python code to determine the type of objects
+
+# declaring objects and assigning values
+a = 10
+b = 10.23
+c = "Hello"
+d = (10, 20, 30, 40)
+e = [10, 20, 30, 40]
+
+# printing types of the objects
+# using type() function
+print("type(a): ", type(a))
+print("type(b): ", type(b))
+print("type(c): ", type(c))
+print("type(d): ", type(d))
+print("type(e): ", type(e))
+
+# printing the type of the value
+# using type() function
+print("type(10): ", type(10))
+print("type(10.23): ", type(10.23))
+print("type(\"Hello\"): ", type("Hello"))
+print("type((10, 20, 30, 40)): ", type((10, 20, 30, 40)))
+print("type([10, 20, 30, 40]): ", type([10, 20, 30, 40]))
