481eff8de5c9d065cc5567c65778dfbee1369c47
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-07 22:10:19 +0530

    initial commit

---
+++ Practicals/C++/new.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+sum(int i=9,int j=3)
+{
+    return (i+j);
+}
+int main()
+
+{
+   int a=0;
+   a=sum(5,9);
+   cout<<"sum is"<<a;
+   cout<<"sum is"<< sum();
+   cout<<"sum is"<< sum(5);
+   cout<<"sum is "<< sum( 8);
+} 
