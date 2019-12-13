7b7b1367c075c7b85ffbad6824528cd4b68b1ff3
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-13 23:14:22 +0530

    initial commit

---
+++ Python/Basic programs/Create integer by assigning binary value.py
@@ -0,0 +1,28 @@
+a=0b1010
+b=0b00000000
+c=0b11111111
+d=0b11110000
+e=0b10101010
+
+print("type of the variables ")
+print("type of a :",type(a))
+print("type of b :",type(b))
+print("type of c :",type(c))
+print("type of d :",type(d))
+print("type of e :",type(e))
+
+print("Value of the variable in decemal format ...")
+print("value of a :",a)
+print("value of b :",b)
+print("value of c :",c)
+print("value of d :",d)
+print("value of e :",e)
+
+print("Value of the variable in binary format ...")
+print("Value of a :",bin(a))
+print("Value of b :",bin(b))
+print("Value of c :",bin(c))
+print("Value of d :",bin(d))
+print("Value of e :",bin(e))
+
+
