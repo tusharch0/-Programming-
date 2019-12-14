9f953ce50ea5d6f4f7d7926b80ff7c24ba206e6d
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-14 21:37:05 +0530

    initial commit

---
+++ Python/Basic programs/Create integer variable by assigning hexadecimal value.py
@@ -0,0 +1,26 @@
+a= 0x123
+b= 0X123
+c= 0xAFAF
+d= 0Xafaf
+e= 0x7890abcdef
+
+print("type of the variable ...")
+print("type of a: ",type(a))
+print("type of b: ",type(b))
+print("type of c: ",type(c))
+print("type of d: ",type(d))
+print("type of e: ",type(e))
+
+print("value of the variable in decimal format ...")
+print("value of a: ",a)
+print("value of b: ",b)
+print("value of c: ",c)
+print("value of d: ",d)
+print("value of e: ",e)
+
+print("value of the variable in octal format ...")
+print("value of a: ",hex(a))
+print("value of b: ",hex(b))
+print("value of c: ",hex(c))
+print("value of d: ",hex(d))
+print("value of e: ",hex(e))
