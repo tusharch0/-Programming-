2b02403afd4a0f2ca9d5a93537d7fef0ae45a221
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-14 21:23:14 +0530

    initial  commit

---
+++ Python/Basic programs/Create integer by assigning octal value.py
@@ -0,0 +1,26 @@
+a= 0o1234567
+b= 0o7654321
+c= 0o1745
+d= 0o100
+e= 0o123
+
+print("type of the variable ...")
+print("type of a: ",type(a))
+print("type of b: ",type(b))
+print("type of c: ",type(c))
+print("type of d: ",type(d))
+print("type of e: ",type(e))
+
+print("value of the variable in decimal format ...")
+print("value of a: ",a)
+print("value of b: ",b)
+print("value of c: ",c)
+print("value of d: ",d)
+print("value of e: ",e)
+
+print("value of the variable in octal format ...")
+print("value of a: ",oct(a))
+print("value of b: ",oct(b))
+print("value of c: ",oct(c))
+print("value of d: ",oct(d))
+print("value of e: ",oct(e))
