854f94f516eedc74da800d10a776b607c24512a8
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-15 21:13:56 +0530

    initial commit

---
+++ Python/Basic programs/Print ASCII value of a character.py
@@ -0,0 +1,6 @@
+char_var ='A'
+print("ASCCI value of "+ char_var + "is = ",ord(char_var))
+char_var = 'x'
+print("ASCCI value of "+ char_var + "is = ",ord(char_var))
+char_var = '9'
+print("ASCCI value of "+ char_var + "is = ",ord(char_var))
