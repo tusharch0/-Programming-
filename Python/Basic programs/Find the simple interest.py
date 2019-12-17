acc31fc12612111f5efef0415fde566c800002f6
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-17 23:13:02 +0530

    initial commit

--- Python/Basic programs/Find the simple interest.py
+++ Python/Basic programs/Find the simple interest.py
@@ -3,6 +3,6 @@ r = float(input("Enter the rate of intrest : "))
 t = float(input ("Enter the time in year : "))
 si=(p*r*t)/100
 print ("Principle amount : ",p)
-print ("Intrest rate :",r)
+print ("Intrest rate : ",r)
 print ("time in years : ",t)
 print ("Sample Intrest : ",si)
