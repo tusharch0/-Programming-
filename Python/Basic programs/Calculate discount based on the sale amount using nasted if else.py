bf5aadbc72ea60cba12e3e00ed0a5bfe1c4009d8
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-20 13:20:46 +0530

    initial commit

---
+++ Python/Basic programs/Calculate discount based on the sale amount using nasted if else.py
@@ -0,0 +1,18 @@
+amt = int (input ("Enter sales amount : "))
+
+if (amt >0):
+    if amt <=5000:
+        disc =amt *0.05
+    else :
+        if amt <=15000:
+            disc=amt *0.12
+        else:
+            if amt <=25000:
+                disc =0.2*amt 
+            else:
+                disc =0.3*amt 
+
+    print ("Discount : ",disc ) 
+    print ("Net pay : ",amt-disc )
+else :
+    print("Invalid Amouunt")
