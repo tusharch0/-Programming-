7798dd44d57274f28959d1a96665b617b1625eac
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-20 15:40:30 +0530

    initial commit

---
+++ Python/Basic programs/Calculate Square of a given number using 3 different method.py
@@ -0,0 +1,8 @@
+number = int(input ("Enter an integer number :"))
+square =number *number 
+print("Square of {0} is {1} ".format (number ,square ))
+
+# By using exponental operator 
+number = int (input("Enter an integer number :"))
+square = number**2
+print ("square of {0} is {1}".format (number ,square))
