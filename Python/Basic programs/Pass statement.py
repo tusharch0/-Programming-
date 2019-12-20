2cd6933c6f6e214fb9fb88698ba2dfe9a862e5f0
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-20 15:19:50 +0530

    initial commit

---
+++ Python/Basic programs/Pass statement.py
@@ -0,0 +1,10 @@
+def hello():
+    pass
+hello()
+
+for i in range(1 ,11):
+    if (i==6):
+        continue 
+    else :
+        pass 
+    print(i) 
---
+++ Python/Basic programs/Print all the numbers from 1 to 1000 which are divisible by 7 and must not be divisible by 5.py
@@ -0,0 +1,5 @@
+begin =1 
+end =1000
+for cnt in range (begin ,end +1):
+    if (cnt %7==0 and cnt %5!=0):
+        print (cnt )
