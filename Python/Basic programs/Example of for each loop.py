fb9a42d6f4b17780bfb2552ddcb2c096fcb867ab
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-20 14:30:22 +0530

    initial commit

---
+++ Python/Basic programs/Example of for each loop.py
@@ -0,0 +1,4 @@
+fruits =["apple","mango","guava","grapes","pineapple"]
+print(type (fruits))
+for fruit in fruits :
+    print (fruit)
---
+++ Python/Basic programs/For loop.py
@@ -0,0 +1,15 @@
+print("Type 1")
+for i in range (10):
+    print(i,end =" ")
+
+print("\nType 2")
+for i in range (1,11):
+    print(i,end= " ")
+
+print("\ntype 3 ")
+for i in range (1,11,3):
+    print(i,end =" ")
+
+print("\n type 4 ")
+for i in range (10,0,-2):
+    print(i,end =" ")
