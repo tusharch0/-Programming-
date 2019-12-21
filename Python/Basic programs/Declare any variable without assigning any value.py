41e717c5ea78a7cf7d64296e8907e92c7fba0cfc
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-21 11:17:45 +0530

    initial commit

---
+++ Python/Basic programs/Declare any variable without assigning any value.py
@@ -0,0 +1,12 @@
+num= None
+print ("Value of num : ",num)
+if (num==None ):
+    print ("Nothing ")
+else : 
+    print ("Somthing ")
+num =100
+print ("value od num : ",num)
+if (num==None):
+    print ("Nothing ")
+else :
+    print ("Somthing ")
---
+++ Python/Basic programs/Write function to find square and cube of the given number.py
@@ -0,0 +1,7 @@
+def square (num):
+    return (num*num)
+def cube (num):
+    return (num*num*num)
+number =int (input ("Enter an integer number : "))
+print ("Squyare of {0} is {1}".format(number ,square(number)))
+print ("Cube of {0} is {1}".format (number ,cube(number )))
