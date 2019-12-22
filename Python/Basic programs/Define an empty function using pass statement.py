49f21eccde813e87708f06f4366efde5d0dd0e9b
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-22 08:42:01 +0530

    initial commit

---
+++ Python/Basic programs/Define an empty function using pass statement.py
@@ -0,0 +1,9 @@
+def myFunc():
+    pass
+def urfunc():
+    print("This is your function")
+
+print("calling function ")
+myFunc()
+urfunc()
+print("End of the program")
---
+++ Python/Basic programs/Find largest of the three number using nested if else.py
@@ -0,0 +1,16 @@
+a =int (input ("Enter A: "))
+b =int (input ("Enter B: "))
+c =int (input ("Enter C: "))
+
+if a>b:
+    if a>c:
+        g=a
+    else:
+        g=c
+else :
+    if b>c:
+        g=b
+    else :
+        g=c
+
+print ("Greater = ",g)
