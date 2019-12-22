8a677735b0ad9e651ea919237480b53876871b1e
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-22 15:24:32 +0530

    initial commit

---
+++ Python/Basic programs/Generate random integer between 0 and 9.py
@@ -0,0 +1,10 @@
+# using randrange
+import random
+for i in range (10):
+   print (random.randrange (10))
+print ("\n")
+
+# using randint
+for i in range(10):
+    print (random.randint(0,10))
+print ("\n")
