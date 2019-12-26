c433aa53040e26e9195178b7823160f88c4de8d0
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-26 21:29:49 +0530

    initial commit

---
+++ Python/Basic programs/Design a biased coin flip function.py
@@ -0,0 +1,12 @@
+import random 
+def biasedcoin():
+    return random.choice (['H','T','H'])
+
+print ('Coin Flip : ',biasedcoin())
+print ('Coin Flip : ',biasedcoin())
+print ('Coin Flip : ',biasedcoin())
+print ('Coin Flip : ',biasedcoin())
+print ('Coin Flip : ',biasedcoin())
+print ('Coin Flip : ',biasedcoin())
+print ('Coin Flip : ',biasedcoin())
+print ('Coin Flip : ',biasedcoin())
---
+++ Python/Basic programs/Design a biased dice throw function.py
@@ -0,0 +1,13 @@
+import random 
+def biaseddice():
+    return random.choice ([1,2,3,4,4,4,5,6,6,6,6])
+
+print ('Dice Threw : ',biaseddice())
+print ('Dice Threw : ',biaseddice())
+print ('Dice Threw : ',biaseddice())
+print ('Dice Threw : ',biaseddice())
+print ('Dice Threw : ',biaseddice())
+print ('Dice Threw : ',biaseddice())
+print ('Dice Threw : ',biaseddice())
+print ('Dice Threw : ',biaseddice())
+print ('Dice Threw : ',biaseddice())
