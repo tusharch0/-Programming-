28e8c829fffda82271738754221b44b1bea4c75e
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-12-27 16:28:56 +0530

    initial commit

---
+++ Python/Basic programs/Find greatest integer using floor() method.py
@@ -0,0 +1,5 @@
+import math 
+num = float (input ("Enter any float number : "))
+print ("math.floor(num): ",math.floor(num))
+num = float (input ("Enter another float number : "))
+print("main.floor (num): ",math.floor(num))
