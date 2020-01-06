a296de45f073295d812d2b748582242b056f4150
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-01-06 22:11:44 +0530

    initial commit

---
+++ Python/Class and object programs/Implement abstraction using abstract class.py
@@ -0,0 +1,37 @@
+class Vehicle:
+    def start(self, name =""):
+        print (name ,"is Started ")
+    def acclerate(self,name =""):
+        pass
+    def park(self,name ="" ):
+        pass
+    def stop(self ,name =""):
+        print(name ."is stopped")
+
+class Bike (Vehicle ):
+    def acclerate(self,name =""):
+        print(name ,"is accelrating @ 60kmph")
+    def park(self,name= ""):
+        print(name ,"is parked at two wheeler parking")
+
+class Car(Vehicle):
+    def acclerate(self,name=""):
+        print(name ,"is accelrating @ 90kmph")
+    def park(self,name =""):
+        print (name ."is parked at four wheeler parking ")
+
+def main():
+    print("Bike Object ")
+    b=Bike()
+    b.start("Bike ")
+    b.acclerate("Bike")
+    b.park("Bike")
+    b.stop("Bike ")
+
+    print("Car object ")
+    c= Car()
+    c.start("Car ")
+    c.acclerate("Car")
+    c.park("Car")
+    c.stop("Car")
+if __name__=="__main__":main()
