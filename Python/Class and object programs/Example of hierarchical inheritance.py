6a4e013f334724ab1b999ac5ae86750e281facb8
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-01-10 23:31:04 +0530

    initial commit

---
+++ Python/Class and object programs/Example of hierarchical inheritance.py
@@ -0,0 +1,52 @@
+class Details :
+    def __init__(self ):
+        self .__id ="<No Id>"
+        self .__name ="<No Name >"
+        self .__gender ="<No Gender>"
+    def setData(self,id,name,gender ):
+        self .__id=id
+        self .__name =name 
+        self .__gender =gender
+    def showData(self ):
+        print("Id : ",self.__id)
+        print("Name :",self.__name )
+        print("Gender : ",self.__gender )
+
+class Employee(Details):
+    def __init__(self):
+        self.__company ="<No company >"
+        self.__dept ="<No Dept >"
+    def setEmployee(self,id,name,gender,comp,dept):
+        self.setData()
+        self.__company=comp
+        self.__dept =dept
+    def showEmployee(self):
+        self.setData()
+        print("Company :",self.__company)
+        print("Department :",self.__dept)
+
+class Doctor(Details):
+    def __init__(self ):
+        self .__hospital ="<No Hospital>"
+        self .__dept ="<No Dept >"
+    def setEmployee(self,id ,name ,gender,hos,dept ):
+        self.setData(id,name,gender)
+        self .__hospital =hos 
+        self .__dept =dept
+    def showEmployee(self ):
+        self.showData()
+        print("Hospital: ",self.__hospital)
+        print("Department: ",self.__dept)
+
+def main():
+    print("Employee Object")
+    e=Employee()
+    e.setEmployee(1,"Tushar","Male","gmr","excavation")
+    e.showEmployee()
+    print("\nDoctor Object ")
+    d=Doctor()
+    d.setEmployee(1,"pankaj","male","aiims","eyes")
+    d.showEmployee()
+
+if __name__ =="__main__":
+    main()
