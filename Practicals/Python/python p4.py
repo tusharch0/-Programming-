bce7da8e874ec7280f974e826f0b69aef48f52bb
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-01-17 23:15:09 +0530

    initial commit

---
+++ Practicals/Python/python p4.py
@@ -0,0 +1,11 @@
+a=10
+b=10.47
+c=10+2j
+
+print("type(a): ",type(a))
+print("type(b): ",type(b))
+print("type(c): ",type(c))
+
+print ("value of a : ",a)
+print ("value of b : ",b)
+print ("value of c : ",c)
