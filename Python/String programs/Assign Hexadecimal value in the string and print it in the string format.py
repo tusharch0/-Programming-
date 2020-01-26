6762f41c93d40333a7d181c772eeb16e2ffdf36c
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-01-26 23:06:13 +0530

    initial commit

---
+++ Python/String programs/Assign Hexadecimal value in the string and print it in the string format.py
@@ -0,0 +1,5 @@
+str1 = "\x41\x42\x43\x44"
+str2 = "This is \x74\x75\x73"
+
+print("Str 1: ",str1)
+print("Str 2: ",str2)
---
+++ Python/String programs/Check if a substring presents in a string using in operator.py
@@ -0,0 +1,14 @@
+str = "Hello_world"
+sub_str = "_"
+
+if sub_str  in str :
+    print("Yes, substring present in the string ")
+else :
+    print("No, substring does not present in the string ")
+
+sub_str = "Hello"
+
+if sub_str in str :
+    print("Yes, substring present in the string ")
+else :
+    print("No, substring does not present in the string ")
