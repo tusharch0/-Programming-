6113a1e25a40f26f341f6900d3fefa91ef50fc74
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-01-29 22:32:59 +0530

    initial commit

---
+++ Practicals/Python/Class/python C10(print quotient and remainder).py
@@ -0,0 +1,5 @@
+a= int (input("Enter the first number : "))
+b= int (input("Enter the Second number : "))
+
+print("Quotient : ",a/b)
+print("Reminder : ",a%b)
---
+++ Practicals/Python/Class/python C11(odd number within a given range).py
@@ -0,0 +1,5 @@
+a= int(input ("Enter the starting number :")) 
+b= int (input ("Enter the limit :"))
+for i in range(a,b,1):
+    if (i%2!=0):
+        print(i)
