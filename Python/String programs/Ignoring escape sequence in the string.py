3bff6007fcaa4108c8c7b7bf4c20bc38aaea6eb2
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-02-03 22:48:56 +0530

    initial commit

---
+++ Python/String programs/Ignoring escape sequence in the string.py
@@ -0,0 +1,13 @@
+# printing single quote 
+str1 = "Hi, I\'m Tushar"
+# printing double quotes 
+str2 = "\"Hello world\""
+# printing path
+str3 = "D:\\work_folder\\python_works"
+# using hexadecimal values 
+str4 = "This is \x74\x75\x73"
+
+print(str1)
+print(str2)
+print(str3)
+print(str4)
