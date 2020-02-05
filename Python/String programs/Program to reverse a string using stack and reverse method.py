deccb3bb4d3a20adffe0bc7be606f8b3d5b2609d
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-02-05 23:53:31 +0530

    initial commit

--- Python/String programs/Program to reverse a string using stack and reverse method.py
+++ Python/String programs/Program to reverse a string using stack and reverse method.py
@@ -7,3 +7,47 @@ def push (element,size,stack):
     else :
         top +=1
         stack[top] = element 
+
+def pop():
+    global top 
+    if top <0:
+        print('Stack Underflow')
+        sys.exit()
+    else:
+        element = stack[top]
+        print('%s'%element ,end='')
+        top -=1
+
+def reverse_by_sort(string):
+    string = list(string)
+    rev_str =''
+    for i in reversed(string):
+        rev_str+=i
+
+    return rev_str
+
+if __name__=='__main__':
+    size =11
+    stack =[0]*size 
+    string = 'Hello_world'
+    top =-1
+
+    push('H', 11, stack)
+    push('e', 11, stack)
+    push('l', 11, stack)
+    push('l', 11, stack)
+    push('o', 11, stack)
+    push('_', 11, stack)
+    push('w', 11, stack)
+    push('o', 11, stack)
+    push('r', 11, stack)
+    push('l', 11, stack)
+    push('d', 11, stack)
+
+    print("Original String = %s"%string )
+    print('\nUsing Stack')
+    print('Reversed String = ',end='')
+    for i in stack:
+        pop()
+    print('\n\nUsing sort()')
+    print('Reverse string = %s'%reverse_by_sort(string))
