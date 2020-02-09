f44dfd98eed2aa8bb481c4c095fa73efb7562a73
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-02-09 23:43:38 +0530

    initial commit

--- Practicals/C++/new.cpp
+++ Practicals/C++/new.cpp
@@ -1,48 +1,7 @@
-// inline function default arguments 
-
-# include <iostream>
-using namespace std ;
-
-int main ()
+#include <iostream>
+using namespace std;
+int main()
 {
-    int i=9,j=3;
-    cout << "Enter the number of compound of the programming ";
-    cin>>i>>j;
-    cout <<  "Enter the power of the product by now ";
-    sum = m+n/2
-    cout <<"sum is : "<<sum;
-    data pair template denorm_indeterminate;
-    has_unique_object_representations_v __JMPSTUB__  __gthr_win32_key_create
-    ifstream template
-    generic_categoryd
-    sscanfs
-    false ata else if (pro)
-    {
-        /* code */
-        template temp ;
-        realloc yet qsort quick sort 
-        sorted protected
-        abort scanf decltype wcerr
-        dynamic_cast disjunction
-    }
-    is_assignable __is_move_assignable_impl
-    print("team number of the varible");
-    NTDDI_VERSION_FROM_WIN32_WINNT 
-    perror
-    remove_reference for (  i = 0; i < count; i++)
-    {
-        /* code */
-        template in the fopen
-        protected   ;
-        pointer_to_binary_function
-        WEOF oct __pentium hem of the 
-        return;
-    }
-    temp return(WEOF)
-     
-}  
-
-void find_pro(int a)
-{
-
+    cout<<"Hello world ";
+    return 0;
 }
