7a48c2a12475577f095765e38c7ccfae27e51e3e
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-02-15 23:30:21 +0530

    initial commit

---
+++ Practicals/Tkinter/program 10.py
@@ -0,0 +1,15 @@
+import tkinter
+from tkinter import ttk  # Necessary in all but this trivial example.
+
+
+def main():
+    root = tkinter.Tk()
+    root.mainloop()
+
+    print('Done with the Event Loop')  # Note when this line runs.
+
+
+# ----------------------------------------------------------------------
+# Calls  main  to start the ball rolling.
+# ----------------------------------------------------------------------
+main()
