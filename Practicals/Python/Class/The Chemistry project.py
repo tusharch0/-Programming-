4378326225b326f471c9be8a6dd3b97920b47b42
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-02-21 22:25:02 +0530

    initial commit

---
+++ Practicals/Python/Class/The Chemistry project.py
@@ -0,0 +1,42 @@
+# take variable 
+# the make chemisrty(property which are similar ) for the variable, structure the which completly stisfy the max chemistry will be the actal structure 
+# it contains path and container 
+# basically a software to implement that
+# path will turn into black to green to darker green
+# from ea web page 
+# @ fifarenderz
+# @ futhead 
+
+Element_symbol =
+Element_name =
+Atomic_number =
+Electron =
+Prtoton =
+Neutron =
+EPN = 
+CAS_number =
+Atomic_weight =
+Density =
+Melting_point =
+Boiling_point =
+Phase =
+Valency =
+Oxidation_states = 
+Ionisation_potential =
+Atomic_Radius = 
+Covatent_Radius =
+VanDerWaals_Radius =
+Electronegativity =
+Electron affinity =
+Electrical_conductivity =
+Electrical_type =
+Magnetic_type =
+Magnetic_susceptibility_volume =
+Magnetic_susceptibility_mass =
+Magnetic_susceptibility_molar =
+Resistivity =
+Superconducting_point =
+Crystal_structure =
+Grid_parameter =
+Attitude =
+Debye_temperature =
