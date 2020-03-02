49ff44917563bba4558a0661854fb8364177294a
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-02 23:43:42 +0530

    initial commit

---
+++ Practicals/Python/python p8.py
@@ -0,0 +1,12 @@
+# Write a program to count the numbers of characters in the string and store them in a dictionary data structure
+str = "Hello World"
+str1 = {}
+for i in str:
+    if i in str1:
+        str1[i] += 1
+    else:
+        str1[i] = 1
+print(str1)
+
+
+# Write a program combine lists that combines these lists into a dictionary.
