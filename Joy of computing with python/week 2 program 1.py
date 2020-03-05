e919bf9bd20d668126df3827f22bffdb1a6eeb51
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-05 23:53:30 +0530

    initial commit

---
+++ Joy of computing with python/week 2 program 1.py
@@ -0,0 +1,21 @@
+#In this assignment, your task is just to read the input number and print it.
+#Before attempting the programming assignments, please make sure you have gone through how to attempt the programming assignments.
+
+#Input:
+#The input will contain only one number.
+
+#Output:
+#Output the same number.
+
+#Example:
+
+#Input:
+#10
+
+#Output:
+#10
+
+#How to take input in python:
+
+a = int(input())  
+print(a)
