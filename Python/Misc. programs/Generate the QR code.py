c3bef595027316102b830ffdda41c843eafd90ee
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-11 21:12:39 +0530

    initial commit

---
+++ Python/Misc. programs/Generate the QR code.py
@@ -0,0 +1,8 @@
+# importing the module
+import qrcode
+
+# information
+First_qrcode = qrcode.make('Includehelp is one of the best sites to learn any programming language from the basics. To visit the site click on the link:  https://www.includehelp.com .')
+
+# to see the QR code on the computer screen
+First_qrcode.show()
