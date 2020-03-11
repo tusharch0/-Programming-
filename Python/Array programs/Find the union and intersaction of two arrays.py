da57c52501b8f557862e405ea910fb271d0d8124
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-11 20:10:35 +0530

    initial commit

--- Python/Array programs/Find the union and intersaction of two arrays.py
+++ Python/Array programs/Find the union and intersaction of two arrays.py
@@ -2,7 +2,7 @@ a = list(map(int ,input ("Enter element of first list : ").split()))
 b = list(map(int ,input ("Enter element of second list : ").split()))
 
 A = list(set(a)|set(b))
-B = list(set(a)&)set(b))
+B = list(set(a)&set(b))
 
 print('Union of the array :',A)
 print("intersection of the array :",B)
