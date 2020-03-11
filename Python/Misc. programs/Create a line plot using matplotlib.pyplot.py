142c3d12eaf87cec988c79c1f84af3176551a4fb
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-11 21:12:11 +0530

    initial commit

---
+++ Python/Misc. programs/Create a line plot using matplotlib.pyplot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [3, 9, 6, 12, 5, 1, 10, 5, 4, 9]
+plt.plot(x, y, label='lineplots', color='b')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Line Plot')
+plt.legend()
+plt.show()
