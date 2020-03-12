dd1be613d2941c1fde4fd34bb2ec03aef790331d
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-12 20:56:41 +0530

    initial commit

---
+++ Python/List programs/Find the positon of minimum and maximum element of a list.py
@@ -0,0 +1,11 @@
+# declare a list of Integers
+list = [10, 1, 2, 20, 3, 20]
+
+# min element's position/index
+min = list.index(min(list))
+# max element's position/index
+max = list.index(max(list))
+
+# printing the position/index of min and max elements
+print "position of minimum element: ", min
+print "position of maximum element: ", max
