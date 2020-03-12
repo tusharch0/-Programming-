e7c7730e79913942bb6f313efa8b43bf1734b656
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-12 20:29:50 +0530

    initial commit

---
+++ Python/List programs/Adding removing elements in the list.py
@@ -0,0 +1,22 @@
+# Declaring a list with integer and string elements
+list = [10, 20, 30, "New Delhi", "Mumbai"]
+
+# printing list
+print "List elements are: ", list
+
+# adding elements
+list.append(40)
+list.append(50)
+list.append("Chennai")
+
+# printing list after adding elements
+print "List elements: ", list
+
+# removing elements
+list.pop()
+# printing list
+print "List elements: ", list
+# removing elements
+list.pop()
+# printing list
+print "List elements: ", list
