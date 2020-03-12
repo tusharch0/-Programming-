ab268439a7e87e88b30166003cac6daf43660398
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-12 20:33:00 +0530

    initial commit

---
+++ Python/List programs/Declare and print a list.py
@@ -0,0 +1,40 @@
+# declaring list with integer elements
+list1 = [10, 20, 30, 40, 50]
+
+# printing list1
+print "List element are: ", list1
+
+
+# printing elements of list1 by index
+print "Element @ 0 index:", list1[0]
+print "Element @ 1 index:", list1[1]
+print "Element @ 2 index:", list1[2]
+print "Element @ 3 index:", list1[3]
+print "Element @ 4 index:", list1[4]
+
+
+# declaring list with string elements
+list2 = ["New Delhi", "Mumbai", "Chennai", "calcutta"]
+
+# printing list2
+print "List elements are: ", list2
+
+#printing elements of list2 by index
+print "Element @ 0 index:", list2[0]
+print "Element @ 1 index:", list2[1]
+print "Element @ 2 index:", list2[2]
+print "Element @ 3 index:", list2[3]
+print " "  # prints new line
+
+# declaring list with mixed elements
+list3 = ["Amit Shukla", 21, "New Delhi", 9876543210]
+
+#printing list3
+print "List elements are: ", list3
+
+# printing elements of list3 by index
+print "Element @ 0 index (Name) :", list3[0]
+print "Element @ 1 index (Age ) :", list3[1]
+print "Element @ 2 index (City) :", list3[2]
+print "Element @ 3 index (Mob.) :", list3[3]
+print ""  # prints new line
