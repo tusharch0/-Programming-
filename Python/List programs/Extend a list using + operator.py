e45256dc5b9b5ab61e5a003fc31718e0b9c6aadc
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-12 20:32:43 +0530

    initial commit

---
+++ Python/List programs/Extend a list using + operator.py
@@ -0,0 +1,11 @@
+# declaring a list with some of the elements
+list1 = [10, 20, 30, 40, 50]
+# printing the list
+print(list1)
+
+# inserting the elements in list
+list1 += [60]
+list1 += [70, 80]
+
+# print the updated list
+print(list1)
