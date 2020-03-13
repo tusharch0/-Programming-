2d0979c1065c4c87cd666e8478d0f4d0abfb2e56
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-13 20:32:30 +0530

    initial commit

---
+++ Python/List programs/Print the index of first matched element of a list.py
@@ -0,0 +1,17 @@
+# declare a list of Integers
+list = [10, 20, 10, 20, 30, 40, 50]
+
+# printing index of 10
+print(list.index(10))
+
+#printing index of 20
+print(list.index(20))
+
+# printing index of 30
+print(list.index(30))
+
+# printing index of 40
+print(list.index(40))
+
+# printing index of 50
+print(list.index(50))
