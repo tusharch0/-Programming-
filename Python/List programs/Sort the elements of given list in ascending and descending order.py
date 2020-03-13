934b0933a9c98257d8f122558c34f38715cfb427
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-13 20:33:22 +0530

    initial commit

---
+++ Python/List programs/Sort the elements of given list in ascending and descending order.py
@@ -0,0 +1,20 @@
+# List of integers
+num = [10, 30, 40, 20, 50]
+
+# sorting and printing
+num.sort()
+print(num)
+
+# List of float numbers
+fnum = [10.23, 10.12, 20.45, 11.00, 0.1]
+
+# sorting and printing
+fnum.sort()
+print(fnum)
+
+# List of strings
+str = ["Banana", "Cat", "Apple", "Dog", "Fish"]
+
+# sorting and  printing
+str.sort()
+print(str)
