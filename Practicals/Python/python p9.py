d26eca20f6a4a36b37338e093b3a47b829f381ba
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-15 23:46:05 +0530

    initial commit

---
+++ Practicals/Python/python p9.py
@@ -0,0 +1,5 @@
+# Write a program to find the most frequent words in a text read from a file. Can you use character frequency to tell whether the given file is a python program file, C program file or a text file.
+
+
+# Write a program to print each line of a file in reverse order. Write a program to compute the number of character, words and lines in a file.
+
