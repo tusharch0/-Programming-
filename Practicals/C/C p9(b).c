323684016253e266f2bbdba35e601aafa822a8f5
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-16 23:10:13 +0530

    initial commit

---
+++ Practicals/C/C p9(b).c
@@ -0,0 +1,1 @@
+//What will the difference if program 20 is going to be implemented through union. Implement the same program through union and differentiate the output as well as memory allocation.
