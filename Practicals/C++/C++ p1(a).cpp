f16e6808587595db482d321cf736bf26f719c91f
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-17 21:23:42 +0530

    intial commit

--- Practicals/C++/C++ p1(a).cpp
+++ Practicals/C++/C++ p1(a).cpp
@@ -1,1 +1,11 @@
 //Assuming there are 7.481 gallons in a cubic foot, write a program that asks the user to enter a number of gallons, and then displays the equivalent in cubic feet.
+#include <iostream>
+using namespace std;
+int main()
+{
+    float g, f = 0;
+    cout << "Enter the the number of gallons : ";
+    cin >> g;
+    f = float(g / 7.481);
+    cout << "Equivelent cubic feet :" << f;
+}
