e98d749ac01a9ae8baf6d517313751d7702b99bd
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-18 23:53:55 +0530

    initial commit

---
+++ Practicals/Python/python p11.py
@@ -0,0 +1,7 @@
+# Write a Python program to:
+# 1. read a file
+# 2. add backslash(\) before every double quote in the file contents.
+# 3. write it to another file in the same folder
+# 4. print the contents of both the files.
+
+# Write a Python program to count the words in the file using a dictionary(use space as a delimiter), Find unique words and the count of their occurrences(ignoring case). Write the output in another file "words.txt" at the same location.
