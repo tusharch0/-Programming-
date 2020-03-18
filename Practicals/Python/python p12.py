465d45d8835ba7a7ac02dd2576c2b2e76c1399ef
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-18 23:54:07 +0530

    initial commit

---
+++ Practicals/Python/python p12.py
@@ -0,0 +1,5 @@
+# Write a Python program that reads a text file, scrambles the words in the file on following rules and writes the output to a new text file:
+# •	Words less than or equal to 3 characters need not be scrambled
+# •	Don't scramble first and last char so Scrambling can become Srbmnacilg or Srbmnailcg or Sambracilg, i.e letters except first and last can be scrambled in any order
+# •	Punctuation at the end of the word to be maintained as is ie. "Surprising," could become "Spsirnirug" but not "Spsirnirug"
+# •	Following punctuation marks are to be supported - Comma Question mark, Full stop, Semicolon, Exclamation Do this for a file and maintain sequences of lines
