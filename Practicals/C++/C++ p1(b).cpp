3a9aa9d12abe8450d937c3c4b4be19e91030cafe
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-19 23:43:05 +0530

    initial commit

--- Practicals/C++/C++ p1(b).cpp
+++ Practicals/C++/C++ p1(b).cpp
@@ -1,1 +1,15 @@
 //The normal speed of a vehicle is less than 65kmph. If entered speed is less than 65kmph print within speed limit otherwise over speed limit.
+#include <iostream>
+using namespace std ;
+int main()
+{
+    int speed;
+    cout<<"Enter the speed of the vehicle in kmph :";
+    cin>>speed;
+    if (speed>65){
+        cout<<"Over speed limit";
+    }
+    else {
+        cout<<"With in speed limit";
+    }
+}
