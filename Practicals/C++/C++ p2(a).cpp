ea7ce4301df6f40d96df1ef8cf2f541627a95fc2
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-20 23:36:03 +0530

    initial commit

--- Practicals/C++/C++ p2(a).cpp
+++ Practicals/C++/C++ p2(a).cpp
@@ -1,1 +1,18 @@
 //Program to generate the Fibonacci series up to user specified limit.
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 1, n, sum = 1, j, b = 1;
+    cout << "Enter the limit :";
+    cin >> n;
+    cout << i << " ";
+    for (j = 0; sum < n; j++)
+    {
+        cout << sum << " ";
+        sum = b + i;
+        i = b;
+        b = sum;
+    }
+    return 0;
+}
