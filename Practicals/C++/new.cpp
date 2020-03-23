fedabe0931cc447d3f9600c5f792adc9ac668da9
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-23 21:29:59 +0530

    initial  commit

--- Practicals/C++/new.cpp
+++ Practicals/C++/new.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello world ";
+    int i=1, n, sum = 1,j,b=1;
+    cout<<"Enter the limit :";
+    cin>>n;
+    cout<<i<<" ";
+    for (j=0;sum<n;j++){
+        cout<<sum<<" ";
+        sum = b+i;
+        i=b;
+        b=sum;
+    }
     return 0;
 }
