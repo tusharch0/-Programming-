5fd33ed8c8daa6eb2d0a8fc65d292bca4af26040
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-03-25 21:57:19 +0530

    initial commit

---
+++ Practicals/C/deo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <conio.h>
+int main(){
+    int a,b;
+    printf("Enter two numbers\n");
+    scanf("%d%d",&a,&b);
+    printf("The sum of two numbers is %d\n", a + b);
+    printf("The subtraction of two numbers is %d\n", a - b);
+    printf("The Multiplication of two numbers is %d\n", a * b);
+    printf("The devision of two numbers is %d\n",a / b);
+    printf("The Modulus of two numbers is %d\n", a % b);
+    return 0;
+}
