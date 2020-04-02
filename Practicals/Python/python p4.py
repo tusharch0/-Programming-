a7afdf7eb3410d54cfdd0838aca1103d5f05e48b
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-04-02 23:16:03 +0530

    initial commit

--- Practicals/Python/python p4.py
+++ Practicals/Python/python p4.py
@@ -16,7 +16,7 @@ s = "Hello_world"
 print("The original string  is : ", end="")
 print(s)
 
-print("The reversed string(using loops) is : ", end="")
+print("The Reversed string(using loops) is : ", end="")
 print(reverse(s))
 print()
 
