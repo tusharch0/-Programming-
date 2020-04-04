067165d0ca203e733d59d9accd16b03f0608529c
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2020-04-04 22:41:59 +0530

    initial commit

--- Practicals/Python/python demo.py
+++ Practicals/Python/python demo.py
@@ -1,13 +1,26 @@
-def fun(i,f):
-    print(i)
-    if(i==0):
-        f=1
-        fun(i+1,f)
-    if(i == 128):
-        f = -1
-        fun(i-1,f)
-    if(i == 1):
-        fun(i+1,f)
-    if(i == -1):
-        fun(i+1,f)
-print(fun(9))
+# writing python program in various modes and printing and assigning values assigned to the variables.
+
+#assigning the value
+a=7 
+print (a)
+print('a')
+
+# 5=b  --> give error 
+print('a =',a)
+print('a ='+str(a))
+
+# for single value to muliple variable  
+p=q=r=9
+print(p)
+print(q)
+print(r)
+
+# for multiple values to multiple variable 
+x,y,z=1,3,6
+print(x)
+print(y)
+print(z)
+
+no=23
+# UID=18bcs6523  --> give error
+UID ='18BCS6523'
